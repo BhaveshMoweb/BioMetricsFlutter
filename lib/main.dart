5c0f8d2df2034487a6f54771df8e4de04450b64f
Neel Gosaliya <neel.gosaliya@moweb.com>
2022-08-03 14:34:56 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'dart:io';
+
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:app_settings/app_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth/error_codes.dart' as auth_error;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final LocalAuthentication auth = LocalAuthentication();
+  late List<BiometricType> availableBiometrics = [];
+  bool canAuthenticate = false;
+  bool didAuthenticate = false;
+
+  @override
+  void initState() {
+    getBiometricsData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            widgetText(),
+            TextButton(
+                onPressed: () async {
+
+                  if(!didAuthenticate){
+                    try {
+                      didAuthenticate = await auth
+                          .authenticate(
+                          localizedReason:
+                          'Please authenticate to show account balance',
+                          options: const AuthenticationOptions(
+                              useErrorDialogs: true));
+                    } on PlatformException catch (e) {
+                      if (e.code == auth_error.notEnrolled) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Finger Print not enrolled"),
+                        ));
+                      } else if (e.code == auth_error.lockedOut ||
+                          e.code == auth_error.permanentlyLockedOut) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Bio Metrics Not Available"),
+                        ));
+                      } else if(e.code == auth_error.passcodeNotSet){
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Device does not have hardware support for biometrics"),
+                        ));
+                      } else if(e.code == auth_error.notAvailable){
+
+                      }
+                    }
+
+                    setState(() {});
+                  }
+
+                },
+                child: Text(didAuthenticate ? "Authentication done":"Authenticate Here"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> getBiometricsData() async {
+    final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+    canAuthenticate =
+        canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+    availableBiometrics = await auth.getAvailableBiometrics();
+
+    setState(() {});
+  }
+
+  Widget widgetText() {
+    if (availableBiometrics.isNotEmpty) {
+      for (BiometricType type in availableBiometrics) {
+        if (availableBiometrics.contains(BiometricType.face)) {
+          return const Text('Face Detection');
+        } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
+          return const Text('Fingerprint');
+        } else {
+          return Text(type.name);
+        }
+      }
+    } else {
+      return TextButton(
+        child: Text("No Biometrics Found!"),
+        onPressed: () {
+          AppSettings.openLockAndPasswordSettings();
+        },
+      );
+    }
+    return const Text("No Biometrics Found");
+  }
+}
